5a9124313fad5c89cbecac6a1c33cdae671101f1
Matheus Viana <matheusgoldship@gmail.com>
2025-01-15 12:57:44 -0300

    Alteração na descrição deste portifolio!

--- src/app/modules/pages/home/home/home.component.ts
+++ src/app/modules/pages/home/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../../components/header/header/header.component';
 import { KnowledgementComponent } from '../../../components/knowledgement/knowledgement/knowledgement.component';
 import { ExperienceComponent } from '../../../components/experience/experience/experience.component';
@@ -11,6 +11,13 @@ import { ProjectsComponent } from '../../../components/projects/projects/project
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  ngOnInit(): void {
+    this.exibirAlerta();
+  }
+
+    public exibirAlerta(){
+      alert("Saudações, nobre avaliador! Informo, que: Os projetos publicados e vinculados a este portifólio, são hospedados com gerenciamento gratuíto. Logo, o serviço poderá estar inativo no momento da solicitação e levar até 50 segundos para o backend emitir a resposta solicitada apenas na primeira solicitação!");
+    }
 
 }
